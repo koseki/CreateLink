7c9858a99817b3f556d479a4ad3dd03045ee6e36
koseki <koseki@gmail.com>
2010-11-29 18:07:42 +0900

    add Copy Fixer function.

--- extension/content.js
+++ extension/content.js
@@ -2,3 +2,30 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     var sel = window.getSelection();
     sendResponse(sel ? sel.toString() : '');
 });
+
+window.addEventListener('keydown', shortcutCopy, true);
+
+function shortcutCopy(event) {
+    if (event.keyCode != 67) return;
+    var isWin = (navigator.platform.indexOf("Win") != -1);
+    var isMac = (navigator.platform.indexOf("Mac") != -1);
+    if ((! isMac && ! event.ctrlKey) || (isMac && ! event.metaKey)) return;
+    if (isSelected()) return;
+    var crlf  = isWin ? "\r\n" : "\n";
+    var txt   = document.title + crlf + document.location.href + crlf + crlf;
+    chrome.extension.sendRequest({command: "setClipboard", data: txt });
+}
+
+function isSelected() {
+    var sel = window.getSelection();
+    if (sel.rangeCount <= 0) return false;
+    if (sel.rangeCount > 1)  return true;
+
+    var range = sel.getRangeAt(0);
+    if (! range.collapsed) return true;
+    if (range.startContainer != range.endContainer) return true;
+    if (range.startOffset    != range.endOffset)    return true;
+    if (document.activeElement.tagName.toLowerCase() != "body") return true;
+
+    return false;
+}
